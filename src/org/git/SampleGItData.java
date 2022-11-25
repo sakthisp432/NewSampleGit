83698882ec57ec2b52eb5f30c80f5c615e674ffc
sakthisp432 <sakthisp432@gmail.com>
2022-11-25 18:56:51 +0530

    donee

---
+++ src/org/git/SampleGItData.java
@@ -0,0 +1,19 @@
+package org.git;
+
+public class SampleGItData {
+	private void g1() {
+		System.out.println("g1");
+
+	}
+	private void g2() {
+		System.out.println("g2");
+	}
+	private void g3() {
+		System.out.println("g3");
+	}
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
